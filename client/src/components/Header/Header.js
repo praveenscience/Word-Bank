b2fe1c82103490b3493b58e61b98b30cc4bd918d
Praveen Kumar <praveenscience@gmail.com>
2020-09-28 06:29:48 +0100

    Created a Bootstrap Header component.

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = ({ dark, children, className }) => {
+  dark = !!dark ? "dark" : "light";
+  return (
+    <nav
+      className={
+        `navbar navbar-${dark} bg-${dark}` + (className ? " " + className : "")
+      }
+    >
+      <span className="navbar-brand">{children}</span>
+    </nav>
+  );
+};
+
+export default Header;
