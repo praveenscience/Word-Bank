1772566ded4502b3ebe19471f87d3c788e9dbe95
Praveen Kumar <praveenscience@gmail.com>
2020-09-28 06:35:18 +0100

    Added a header with Logo.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
+import Logo from "../assets/logo/logo-light.png";
 
 class App extends Component {
   render() {
-    return <div>Word Bank</div>;
+    return (
+      <div className="App">
+        <Header className="Header" dark={true}>
+          <img src={Logo} alt="Word Bank Logo" />
+          Word Bank
+        </Header>
+      </div>
+    );
   }
 }
 
