a8c4d7675cb03534e497abe9439079b7ee90baf1
Praveen Kumar <praveenscience@gmail.com>
2020-09-30 06:46:47 +0100

    Different Lifecycle Methods.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,41 @@ import Header from "./Header/Header";
 import Logo from "../assets/logo/logo-light.png";
 
 class App extends Component {
+  state = {
+    A: "B"
+  };
+  constructor(props) {
+    super(props);
+    console.log("Constructor called!");
+  }
+  componentWillMount() {
+    console.log("componentWillMount called!");
+  }
+  componentDidMount() {
+    console.log("componentDidMount called!");
+    setTimeout(() => {
+      console.log("Changing State...");
+      this.setState({ A: "A" });
+    }, 1000);
+  }
+  componentWillReceiveProps() {
+    console.log("componentWillReceiveProps called!");
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate called!");
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componentWillUpdate called!");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate called!");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount called!");
+  }
   render() {
+    console.log("Render function called!");
     return (
       <div className="App">
         <Header className="Header fixed-top" dark={true}>
