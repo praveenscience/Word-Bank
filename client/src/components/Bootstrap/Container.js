e2d2c40c8ca3eb9cc2d54191a3c07ebc51233ccf
Praveen Kumar <praveenscience@gmail.com>
2020-10-01 06:06:55 +0100

    Added Bootstrap Containers.

---
+++ client/src/components/Bootstrap/Container.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Container = ({ fluid, className, children }) => {
+  return (
+    <div
+      className={
+        "container" +
+        (fluid ? "-fluid" : "") +
+        (className && className.length > 0 ? " " + className : "")
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
