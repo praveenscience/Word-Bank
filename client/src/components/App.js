af47d13b22bced3a7b0a03e2a807c19e52c241da
Praveen Kumar <praveenscience@gmail.com>
2020-10-01 06:04:01 +0100

    Defined a user state.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,9 @@ import Header from "./Header/Header";
 import Logo from "../assets/logo/logo-light.png";
 
 class App extends Component {
+  state = {
+    User: null
+  };
   render() {
     return (
       <div className="App">
