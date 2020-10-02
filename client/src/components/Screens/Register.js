e7a8541b150fd89fdafbd9ecb1083d90cef4181d
Praveen Kumar <praveenscience@gmail.com>
2020-10-02 07:02:18 +0100

    Created form elements for both login and register.

--- client/src/components/Screens/Register.js
+++ client/src/components/Screens/Register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../Bootstrap/Card";
+import FormGroup from "../Bootstrap/Forms/FormGroup";
 
 const Register = ({ onChange, onSubmit }) => {
   return (
@@ -9,6 +10,41 @@ const Register = ({ onChange, onSubmit }) => {
       Text="Please register to access Word Bank and add new words to the system."
     >
       <form onChange={onChange} onSubmit={onSubmit}>
+        <FormGroup
+          ID="fullname"
+          Label="Your Name"
+          Type="text"
+          Placeholder="Please enter your full name."
+          Desc="Please enter your full name so that we can call you by that."
+        />
+        <FormGroup
+          ID="username"
+          Label="Username"
+          Type="text"
+          Placeholder="Please enter your username."
+          Desc="Please enter your username so that you can use for logging in."
+        />
+        <FormGroup
+          ID="password"
+          Label="Password"
+          Type="password"
+          Placeholder="Please enter your password."
+          Desc="Please enter your password so that you can use for logging in."
+        />
+        <FormGroup
+          ID="confpass"
+          Label="Confirm Password"
+          Type="password"
+          Placeholder="Please enter the same password as above."
+          Desc="Please enter your password again so that it makes your life tougher."
+        />
+        <FormGroup
+          ID="email"
+          Label="Email Address"
+          Type="email"
+          Placeholder="Please enter your email address."
+          Desc="Please enter your email address so we can send you spam."
+        />
         <button type="submit" className="btn btn-primary">
           Register
         </button>
