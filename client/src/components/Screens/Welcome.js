3dbeca0cc5bcda22a0ea0cb9900f152e35f3a410
Praveen Kumar <praveenscience@gmail.com>
2020-10-12 06:23:38 +0100

    Updated Welcome page styles.

--- client/src/components/Screens/Welcome.js
+++ client/src/components/Screens/Welcome.js
@@ -4,16 +4,24 @@ import Card from "../Bootstrap/Card";
 const Welcome = ({ User, onChange, onSubmit }) => {
   return (
     <Card
-      Header="Welcome"
+      Header={
+        <>
+          Welcome
+          <form
+            onChange={onChange}
+            onSubmit={onSubmit}
+            className="card-header-btn"
+          >
+            <button type="submit" className="btn btn-sm btn-danger">
+              Logout
+            </button>
+          </form>
+        </>
+      }
       Title={"Welcome to Word Bank, " + User.FullName}
       Text="Please add your nice words here."
-    >
-      <form onChange={onChange} onSubmit={onSubmit}>
-        <button type="submit" className="btn btn-primary">
-          Logout
-        </button>
-      </form>
-    </Card>
+      className="Welcome"
+    ></Card>
   );
 };
 
