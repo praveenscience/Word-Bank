5cad5d0e34f55aa6672d2f7180812ce6af95a85b
Praveen Kumar <praveenscience@gmail.com>
2020-10-12 06:35:59 +0100

    Created an Intro Page.

---
+++ client/src/components/Screens/Welcome/Intro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <section className="intro">
+      <h3>Welcome to Word Bank</h3>
+      <p>Click on a Word from the list on the left.</p>
+    </section>
+  );
+};
+
+export default Intro;
