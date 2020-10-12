b8493b282b665b3948d895cc7910cf2cc2d482dc
Praveen Kumar <praveenscience@gmail.com>
2020-10-12 06:49:17 +0100

    Task for this day.
    * Convert the object into NavLinks.

--- client/src/components/Screens/Welcome/List.js
+++ client/src/components/Screens/Welcome/List.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Words from "../../../constants/DemoWords";
 
 const List = () => {
   return (
     <div className="list-group">
+      <pre className="border bg-light rounded p-2">
+        {JSON.stringify(Words, null, 2)}
+      </pre>
       <NavLink
-        to="/word/hello"
+        to="/word/Word1"
         className="list-group-item list-group-item-action"
       >
-        Hello
+        Word 1
       </NavLink>
       <NavLink
-        to="/word/world"
+        to="/word/Word2"
         className="list-group-item list-group-item-action"
       >
-        World
+        Word 2
       </NavLink>
     </div>
   );
