516e6ed50e37357251c030fc9625fa178d4eb1f8
Praveen Kumar <praveenscience@gmail.com>
2020-10-12 06:45:11 +0100

    Rendered Demo Words and Error Handling.

--- client/src/components/Screens/Welcome/Word.js
+++ client/src/components/Screens/Welcome/Word.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Words from "../../../constants/DemoWords";
 
 const Word = ({ match }) => {
-  return (
-    <section className="Word">
-      <h3>Word: {match.params.wordId}</h3>
-    </section>
-  );
+  if (Words[match.params.wordId])
+    return (
+      <section className="Word">
+        <h3>Word: {match.params.wordId}</h3>
+      </section>
+    );
+  else
+    return (
+      <section className="Word">
+        <h3>Error 404! Word Not Found</h3>
+      </section>
+    );
 };
 
 export default Word;
