f0d7ea7a4e5f68d6e038fc7ba95778f08ab4985e
Praveen Kumar <praveenscience@gmail.com>
2020-10-13 06:19:16 +0100

    Basic Express App on port 3100.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log("Hello LetsGrad!");
+// Import Express JS.
+const express = require("express");
+// Create an Express Instance.
+const app = express();
+// Set a port.
+const port = 3100;
+// Listen the application to the port.
+app.listen(port, () => {
+  console.log(`Server started in Port ${port}!`);
+});
