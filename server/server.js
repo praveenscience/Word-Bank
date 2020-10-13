f485d9145e7abc67268a8d86094f67aec1a73b34
Praveen Kumar <praveenscience@gmail.com>
2020-10-13 06:42:27 +0100

    Implemented response with Words API.

--- server/server.js
+++ server/server.js
@@ -10,6 +10,22 @@ app.use("/", express.static("static"));
 app.get("/", (req, res) => {
   res.json("Here's WordBank API!");
 });
+app.get("/api/words", (req, res) => {
+  res.json({
+    hello: {
+      Word: "Hello",
+      Meaning: "Used as a greeting or to begin a telephone conversation.",
+      Sentence: "Hello there, LetsGrad!",
+      User: ""
+    },
+    world: {
+      Word: "World",
+      Meaning: "The earth, together with all of its countries and peoples.",
+      Sentence: "He was doing his bit to save the world",
+      User: ""
+    }
+  });
+});
 // Listen the application to the port.
 app.listen(port, () => {
   console.log(`Server started in Port ${port}!`);
