0089d1ed0592a5a147918a30b8f07f1b6cb3f092
Praveen Kumar <praveenscience@gmail.com>
2020-10-13 06:32:45 +0100

    Created a basic route.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const express = require("express");
 const app = express();
 // Set a port.
 const port = 3100;
+// Adding Routes.
+app.get("/", (req, res) => {
+  res.json("Hello LetsGrad!");
+});
 // Listen the application to the port.
 app.listen(port, () => {
   console.log(`Server started in Port ${port}!`);
