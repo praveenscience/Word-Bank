1653a5b19b02655e21d2cf3e0a0d67565c2f16b2
Praveen Kumar <praveenscience@gmail.com>
2020-10-16 05:58:32 +0100

    Enabled sessions on our Express app.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // Import Express JS.
 const express = require("express");
+const session = require("express-session");
 // Import Routes.
 const rootRoute = require("./routes/root");
 const apiRoute = require("./routes/api");
@@ -9,6 +10,19 @@ const app = express();
 const port = 3100;
 // Read the body for post.
 app.use(express.json());
+// Enabling Sessions Support
+const sess = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+};
+// Only if production, enable secure cookies and trust proxy.
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+app.use(session(sess));
 // Routing.
 app.use("/", rootRoute);
 app.use("/api", apiRoute);
