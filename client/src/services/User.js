ce2315f65193433a8e5a49d9b7d5383f824c9382
Praveen Kumar <praveenscience@gmail.com>
2020-10-20 07:07:16 +0100

    Created a service call for User Registration endpoint.

--- client/src/services/User.js
+++ client/src/services/User.js
@@ -2,3 +2,5 @@ import Axios from "axios";
 
 export const UserLogin = (username, password) =>
   Axios.post("/api/users/login", { username, password });
+export const UserRegister = (fullname, username, password, email) =>
+  Axios.post("/api/users", { fullname, username, password, email });
