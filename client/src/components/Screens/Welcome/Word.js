631d88b3921e643e6ded2319bca53796ca6733e0
Praveen Kumar <praveenscience@gmail.com>
2020-10-22 06:46:42 +0100

    Added delete option.

--- client/src/components/Screens/Welcome/Word.js
+++ client/src/components/Screens/Welcome/Word.js
@@ -1,29 +1,70 @@
 import React from "react";
 import moment from "moment";
+import { useState } from "react";
+import { DeleteWord } from "../../../services/Words";
 
 const Word = ({ match, Words }) => {
+  const [DeleteMode, setDeleteMode] = useState(false);
   const WordID = match.params.wordId;
   const Word = Words[WordID];
+  const handleDelete = e => {
+    e.preventDefault();
+    setDeleteMode(!DeleteMode);
+  };
+  const handleReallyDelete = e => {
+    e.preventDefault();
+    DeleteWord(WordID).then(res => {
+      // Tell the user, the word has been deleted.
+      // Take back to the home page.
+      // Update the list of words.
+    });
+  };
   if (Word)
     return (
       <section className="Word">
         <h3>
           Word: <strong>{WordID}</strong>
+          <span className="btn-group float-right">
+            <button className="btn btn-sm btn-danger" onClick={handleDelete}>
+              {DeleteMode ? "Cancel" : "Delete"}
+            </button>
+          </span>
         </h3>
-        <div className="alert alert-info mb-4">
-          This word has been created by <em>{Word.User}</em> on{" "}
-          <em>{moment(Word.DateCreated).format("MMMM Do YYYY, h:mm a")}</em>.
-        </div>
-        <h4>Word</h4>
-        <p>
-          The current word we are learning is <strong>{Word.Word}</strong>.
-        </p>
-        <h4>Meaning</h4>
-        <p>{Word.Meaning}</p>
-        <h4>Example</h4>
-        <p>
-          <em>{Word.Sentence}</em>
-        </p>
+        {DeleteMode ? (
+          <div className="alert alert-danger text-center">
+            <p>
+              Are you sure, you want to delete the word{" "}
+              <strong>{Word.Word}</strong>?
+            </p>
+            <button
+              className="btn btn-danger mr-3"
+              onClick={handleReallyDelete}
+            >
+              Yes, I am sure.
+            </button>
+            <button className="btn btn-info btn-sm" onClick={handleDelete}>
+              No, take me back.
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="alert alert-info mb-4">
+              This word has been created by <em>{Word.User}</em> on{" "}
+              <em>{moment(Word.DateCreated).format("MMMM Do YYYY, h:mm a")}</em>
+              .
+            </div>
+            <h4>Word</h4>
+            <p>
+              The current word we are learning is <strong>{Word.Word}</strong>.
+            </p>
+            <h4>Meaning</h4>
+            <p>{Word.Meaning}</p>
+            <h4>Example</h4>
+            <p>
+              <em>{Word.Sentence}</em>
+            </p>
+          </>
+        )}
       </section>
     );
   else
