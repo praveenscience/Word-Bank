589a7cd59928daa19d08f0a41938f5a01b1e2f02
Praveen Kumar <praveenscience@gmail.com>
2020-10-22 06:19:40 +0100

    Displayed the Word's meta information.

--- client/src/components/Screens/Welcome/Word.js
+++ client/src/components/Screens/Welcome/Word.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Word = ({ match, Words }) => {
-  if (Words[match.params.wordId])
+  const WordID = match.params.wordId;
+  const Word = Words[WordID];
+  if (Word)
     return (
       <section className="Word">
-        <h3>Word: {match.params.wordId}</h3>
+        <h3>
+          Word: <strong>{WordID}</strong>
+        </h3>
+        <div className="alert alert-info">
+          This word has been created by <em>{Word.User}</em> on{" "}
+          <em>{Word.DateCreated}</em>.
+        </div>
       </section>
     );
   else
