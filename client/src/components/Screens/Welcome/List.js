f8a9335559f6db8c0c99b7da145c6a1023266470
Praveen Kumar <praveenscience@gmail.com>
2020-10-22 06:12:59 +0100

    Added an empty state message in List.

--- client/src/components/Screens/Welcome/List.js
+++ client/src/components/Screens/Welcome/List.js
@@ -5,15 +5,23 @@ const List = ({ Words }) => {
   return (
     <>
       <div className="list-group">
-        {Object.keys(Words).map((word, key) => (
-          <NavLink
-            to={"/word/" + word}
-            className="list-group-item list-group-item-action"
-            key={key}
-          >
-            {Words[word].Word}
-          </NavLink>
-        ))}
+        {Object.keys(Words).length > 0 ? (
+          Object.keys(Words).map((word, key) => (
+            <NavLink
+              to={"/word/" + word}
+              className="list-group-item list-group-item-action"
+              key={key}
+            >
+              {Words[word].Word}
+            </NavLink>
+          ))
+        ) : (
+          <div className="list-group-item list-group-item-danger text-center">
+            No Words!
+            <br />
+            Please consider <NavLink to="/">adding a word</NavLink>.
+          </div>
+        )}
       </div>
     </>
   );
