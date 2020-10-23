c2f73c839c0fcc62364494f9b4c4544bb5cb9a93
Praveen Kumar <praveenscience@gmail.com>
2020-10-23 06:18:56 +0100

    Separated Users into Users Constant.

---
+++ server/constants/users.js
@@ -0,0 +1,38 @@
+const Users = {
+  Praveen: {
+    email: "praveen@letsgrad.com",
+    fullname: "Praveen Kumar",
+    password: "Hello@123"
+  },
+  AbhiVikrant: {
+    email: "abhi@letsgrad.com",
+    fullname: "Abhishek Vikrant",
+    password: "1234@123"
+  },
+  Santosh: {
+    email: "santosh@letsgrad.com",
+    fullname: "Santosh",
+    password: "12345"
+  },
+  Ruchita: {
+    email: "ruchita@letsgrad.com",
+    fullname: "Ruchita",
+    password: "Carol@123"
+  },
+  Princy: {
+    email: "princy@letsgrad.com",
+    fullname: "Princy Agarwal",
+    password: "passw"
+  },
+  Nagaraj: {
+    email: "nagaraj@letsgrad.com",
+    fullname: "Nagaraj",
+    password: "nagsvk123"
+  },
+  angle: {
+    email: "angle@letsgrad.com",
+    fullname: "Obtuse Angle",
+    password: "hello@12"
+  }
+};
+module.exports = Users;
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,44 +3,7 @@ const express = require("express");
 const { CheckEmail } = require("../../helpers/validators");
 // Import Express Routes
 const users = express.Router();
-
-const Users = {
-  Praveen: {
-    email: "praveen@letsgrad.com",
-    fullname: "Praveen Kumar",
-    password: "Hello@123"
-  },
-  AbhiVikrant: {
-    email: "abhi@letsgrad.com",
-    fullname: "Abhishek Vikrant",
-    password: "1234@123"
-  },
-  Santosh: {
-    email: "santosh@letsgrad.com",
-    fullname: "Santosh",
-    password: "12345"
-  },
-  Ruchita: {
-    email: "ruchita@letsgrad.com",
-    fullname: "Ruchita",
-    password: "Carol@123"
-  },
-  Princy: {
-    email: "princy@letsgrad.com",
-    fullname: "Princy Agarwal",
-    password: "passw"
-  },
-  Nagaraj: {
-    email: "nagaraj@letsgrad.com",
-    fullname: "Nagaraj",
-    password: "nagsvk123"
-  },
-  angle: {
-    email: "angle@letsgrad.com",
-    fullname: "Obtuse Angle",
-    password: "hello@12"
-  }
-};
+const Users = require("../../constants/users");
 
 // Adding Routes.
 users.get("/", (req, res) => {
