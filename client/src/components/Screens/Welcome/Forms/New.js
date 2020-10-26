b02be4cf5248f25d7efe50f92e52eb67b48970a2
Praveen Kumar <praveenscience@gmail.com>
2020-10-26 06:10:57 +0000

    Created the form and values to be posted.

--- client/src/components/Screens/Welcome/Forms/New.js
+++ client/src/components/Screens/Welcome/Forms/New.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import FormGroup from "../../../Bootstrap/Forms/FormGroup";
 
 const NewWord = () => {
-  return <div>New Word</div>;
+  const [Error, setError] = useState(false);
+  const [Values, setValues] = useState({
+    slug: "",
+    word: "",
+    meaning: "",
+    sentence: ""
+  });
+  const sluggify = word =>
+    word
+      .toLowerCase()
+      .replace(/[\s!"|',#$%^&*()+]/g, "-")
+      .split("-")
+      .filter(a => a)
+      .join("-");
+  const onInputChange = e => {
+    setValues({
+      ...Values,
+      [e.target.name]: e.target.value,
+      slug: e.target.name === "word" ? sluggify(e.target.value) : Values.slug
+    });
+  };
+  return (
+    <section className="Word">
+      <h3>Create a New Word</h3>
+      <p>Use the below form to create a new word.</p>
+      <form>
+        {Error && <div className="alert alert-danger">{Values.Error}</div>}
+        {["Slug", "Word", "Meaning", "Sentence"].map((fg, key) => (
+          <FormGroup
+            key={key}
+            ID={fg.toLowerCase()}
+            ReadOnly={fg === "Slug"}
+            Label={fg}
+            Type="text"
+            Value={Values[fg.toLowerCase()]}
+            Placeholder={"Please enter the " + fg.toLowerCase() + "."}
+            onChange={onInputChange}
+          />
+        ))}
+      </form>
+    </section>
+  );
 };
 
 export default NewWord;
