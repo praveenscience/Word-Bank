a412d6cfd4c4922e67b0bf7438d317d274c50341
Praveen Kumar <praveenscience@gmail.com>
2020-10-26 06:20:15 +0000

    Fixed capitalisation for slug.

--- client/src/components/Screens/Welcome/Forms/New.js
+++ client/src/components/Screens/Welcome/Forms/New.js
@@ -5,7 +5,7 @@ import FormGroup from "../../../Bootstrap/Forms/FormGroup";
 const NewWord = () => {
   const [Error, setError] = useState(false);
   const [Values, setValues] = useState({
-    Slug: "",
+    slug: "",
     Word: "",
     Meaning: "",
     Sentence: ""
@@ -21,7 +21,7 @@ const NewWord = () => {
     setValues({
       ...Values,
       [e.target.name]: e.target.value,
-      Slug: e.target.name === "Word" ? sluggify(e.target.value) : Values.Slug
+      slug: e.target.name === "Word" ? sluggify(e.target.value) : Values.slug
     });
   };
   const handleSubmit = e => {
@@ -36,11 +36,11 @@ const NewWord = () => {
       <p>Use the below form to create a new word.</p>
       <form onSubmit={handleSubmit}>
         {Error && <div className="alert alert-danger">{Values.Error}</div>}
-        {["Slug", "Word", "Meaning", "Sentence"].map((fg, key) => (
+        {["slug", "Word", "Meaning", "Sentence"].map((fg, key) => (
           <FormGroup
             key={key}
             ID={fg}
-            ReadOnly={fg === "Slug"}
+            ReadOnly={fg === "slug"}
             Label={fg}
             Type="text"
             Value={Values[fg]}
@@ -48,6 +48,11 @@ const NewWord = () => {
             onChange={onInputChange}
           />
         ))}
+        <input
+          type="submit"
+          value="Create New Word"
+          className="btn btn-primary"
+        />
       </form>
     </section>
   );
