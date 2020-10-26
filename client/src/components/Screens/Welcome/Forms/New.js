311af9549f24f70ad79904a2b736a6f4dae3ed86
Praveen Kumar <praveenscience@gmail.com>
2020-10-26 06:16:08 +0000

    Fixed capitalisation.

--- client/src/components/Screens/Welcome/Forms/New.js
+++ client/src/components/Screens/Welcome/Forms/New.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+import { CreateWord } from "../../../../services/Words";
 import FormGroup from "../../../Bootstrap/Forms/FormGroup";
 
 const NewWord = () => {
   const [Error, setError] = useState(false);
   const [Values, setValues] = useState({
-    slug: "",
-    word: "",
-    meaning: "",
-    sentence: ""
+    Slug: "",
+    Word: "",
+    Meaning: "",
+    Sentence: ""
   });
   const sluggify = word =>
     word
@@ -20,23 +21,29 @@ const NewWord = () => {
     setValues({
       ...Values,
       [e.target.name]: e.target.value,
-      slug: e.target.name === "word" ? sluggify(e.target.value) : Values.slug
+      Slug: e.target.name === "Word" ? sluggify(e.target.value) : Values.Slug
+    });
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    CreateWord(Values).then(res => {
+      console.log(res);
     });
   };
   return (
     <section className="Word">
       <h3>Create a New Word</h3>
       <p>Use the below form to create a new word.</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         {Error && <div className="alert alert-danger">{Values.Error}</div>}
         {["Slug", "Word", "Meaning", "Sentence"].map((fg, key) => (
           <FormGroup
             key={key}
-            ID={fg.toLowerCase()}
+            ID={fg}
             ReadOnly={fg === "Slug"}
             Label={fg}
             Type="text"
-            Value={Values[fg.toLowerCase()]}
+            Value={Values[fg]}
             Placeholder={"Please enter the " + fg.toLowerCase() + "."}
             onChange={onInputChange}
           />
