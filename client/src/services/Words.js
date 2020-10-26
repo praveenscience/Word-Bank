17ec49fec4d1ca81d05b71d70ddf18835e5405af
Praveen Kumar <praveenscience@gmail.com>
2020-10-26 06:52:30 +0000

    Created a service call for UpdateWord endpoint.

--- client/src/services/Words.js
+++ client/src/services/Words.js
@@ -2,4 +2,5 @@ import Axios from "axios";
 
 export const GetWords = () => Axios.get("/api/words");
 export const CreateWord = Word => Axios.post("/api/words", Word);
+export const UpdateWord = Word => Axios.put("/api/words/" + Word.slug, Word);
 export const DeleteWord = WordID => Axios.delete("/api/words/" + WordID);
