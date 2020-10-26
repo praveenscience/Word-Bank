4439f7ea106c375db41dac4717351605a56b208e
Praveen Kumar <praveenscience@gmail.com>
2020-10-26 07:00:34 +0000

    Implemented a simple search functionality.

--- client/src/components/Screens/Welcome/List.js
+++ client/src/components/Screens/Welcome/List.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const List = ({ Words }) => {
+  const [Search, setSearch] = useState("");
+  const SearchFilter = slug => {
+    return slug.indexOf(Search.toLowerCase()) > -1;
+  };
   return (
     <>
+      <input
+        type="search"
+        className="form-control btn-sm mb-3"
+        placeholder="Search Words..."
+        value={Search}
+        onChange={e => setSearch(e.target.value)}
+      />
       <div className="list-group">
-        {Object.keys(Words).length > 0 ? (
-          Object.keys(Words).map((word, key) => (
-            <NavLink
-              to={"/word/" + word}
-              className="list-group-item list-group-item-action"
-              key={key}
-            >
-              {Words[word].Word}
-            </NavLink>
-          ))
+        {Object.keys(Words).filter(SearchFilter).length > 0 ? (
+          Object.keys(Words)
+            .filter(SearchFilter)
+            .map((word, key) => (
+              <NavLink
+                to={"/word/" + word}
+                className="list-group-item list-group-item-action"
+                key={key}
+              >
+                {Words[word].Word}
+              </NavLink>
+            ))
         ) : (
           <div className="list-group-item list-group-item-danger text-center">
             No Words!
